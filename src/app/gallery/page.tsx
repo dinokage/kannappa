4cb1fc1ac4c55fea16b117d7600f6e9ab28fff98
Dinesh Yerra <68950956+dinokage@users.noreply.github.com>
2025-05-06 14:55:57 +0530

    changed coming soon image

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -9,7 +9,7 @@ export default function GalleryPage() {
   return (
     <div className="flex flex-col min-h-screen">
 
-      <Image src={"/coming-soon.png"} alt="coming soon" width={1000} height={1000} className="object-cover mx-auto"/>
+      <Image src={"/gallery/5070.jpg"} alt="coming soon" width={1000} height={1000} className="object-cover mx-auto"/>
 
       {/* Footer */}
       
